c203914083222429e1345f75aa20ccd264cc54a8
syntaxframe <niiaz280228@gmail.com>
2024-02-16 14:55:36 +0300

    fix: deployment bugs

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 let base_val = '/'
 if(process.env.NODE_ENV === 'production' && process.env.GITHUB_REPOSITORY)
-  base_val = process.env.GITHUB_REPOSITORY.split('/')[1]
+  base_val = `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
 
 // https://vitejs.dev/config/
 export default defineConfig({
